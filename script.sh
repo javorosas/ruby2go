62fe10c9ae49cc1858f1197442e4f536f0fadb25
Ib Quezada <iquezada@nearsoft.com>
2013-01-14 19:02:00 -0700

    Add package manager for various distributions, including its rvm dependencies

--- script.sh
+++ script.sh
@@ -14,21 +14,35 @@ echo -e "\e[1;37mWelcome to Ruby2Go: The best way to install Ruby.\e[0m"    #Pri
 ### Choose the proper package manager ###
 #########################################
 
+pkm_apt_binary="$(     builtin command -v apt-get )"
+pkm_emerge_binary="$(  builtin command -v emerge  )"
+pkm_pacman_binary="$(  builtin command -v pacman  )"
+pkm_yum_binary="$(     builtin command -v yum     )"
+pkm_zypper_binary="$(  builtin command -v zypper  )"
+pkm_pkg_add_binary="$( builtin command -v pkg_add )"
+
 # Checks if evaluated package manager is installed
-haveProg() {
-    [ -x "$(which $1)" ]
-}
 
-assignPacMan() {
-    pack = $1
-}
-
-# The package manager instalation command string is stored in the 'pack' variable
-if haveProg apt-get ; then pack="sudo apt-get install -y"
-elif haveProg yum ; then pack="sudo yum install"
-elif haveProg pacman ; then pack="sudo pacman -S"
+if [[ -n "$pkm_apt_binary" ]]; then
+    pack="sudo apt-get install -y"
+    rvm_dependencies="build-essential openssl libreadline6 libreadline6-dev curl git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt-dev autoconf libc6-dev ncurses-dev automake libtool bison subversion pkg-config"
+elif [[ -n "$pkm_emerge_binary" ]]; then
+    pack="sudo emerge"
+    rvm_dependencies="libiconv readline zlib openssl curl git libyaml sqlite libxslt libtool gcc autoconf automake bison m4"
+elif [[ -n "$pkm_pacman_binary" ]]; then
+    pack="sudo pacman -Sy --noconfirm"
+    rvm_dependencies="gcc patch curl zlib readline libxml2 libxslt git autoconf automake diffutils make libtool bison subversion"
+elif [[ -n "$pkm_yum_binary" ]]; then
+    pack="sudo yum install -y"
+    rvm_dependencies="gcc-c++ patch readline readline-devel zlib zlib-devel libyaml-devel libffi-devel openssl-devel make bzip2 autoconf automake libtool bison iconv-devel"
+elif [[ -n "$pkm_zypper_binary" ]]; then
+    pack="sudo zypper install -y"
+    rvm_dependencies="patterns-openSUSE-devel_basis gcc-c++ bzip2 readline-devel zlib-devel libxml2-devel libxslt-devel libyaml-devel libopenssl-devel libffi45-devel libtool bison"
+elif [[ -n "$pkm_pkg_add_binary" ]]; then
+    pack="sudo pkg_add"
+    rvm_dependencies="gcc curl readline libxml2 libxslt git autoconf automake diffutils make libtool bison subversion"
 else
-    echo 'No package manager found!'
+    echo "No package manager found!"
     exit 2
 fi
 
@@ -59,17 +73,14 @@ install(){
 install "curl"
 install "git"
 
-# TODO: Change the following with rvm requirements
-install "zlib1g-dev"
-install "libyaml-dev"
-install "libsqlite3-dev"
-
 install "nodejs"
 
 # Install RVM with curl
 curl -L get.rvm.io | bash -s stable
 
 source $HOME/.rvm/scripts/rvm
+
+$pack $rvm_dependencies
 
 rvm pkg install openssl
 rvm install 1.9.3 --with-openssl-dir=$HOME/.rvm/usr
